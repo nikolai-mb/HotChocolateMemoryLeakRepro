380b4c95e4fe790b8ccd7904d90a453689816ca1
Nikolai Bjørndalseter / Intility AS <nikolai.bjorndalseter@intility.no>
2023-04-26 18:55:38 +0200

    Simplified load simulation query

--- HotChocolateMemoryLeakRepro/Query.cs
+++ HotChocolateMemoryLeakRepro/Query.cs
@@ -1,5 +1,5 @@
-﻿using System.Text;
-using System.Threading.Channels;
+﻿using System.Diagnostics;
+using System.Text;
 using Microsoft.EntityFrameworkCore;
 
 namespace HCMemoryLeakRepro;
@@ -18,56 +18,42 @@ public class Query
 		HttpClient client = new(); // Socket exhaustion is not a concern here
 
 		var filter = createMemoryLeak
-			? "id: { in: [ 1 ] }"
-			: "id: { eq: 1 }";
+			? "in: [ 1 ]"
+			: "eq: 1";
+		
+		var query = $$"""{ "query": "{ carBrands(where:{ id: { {{filter}} } }) { id name } }" }""";
 
-		var query = $$"""{ "query": "{ carBrands(where:{ {{filter}} }) { id name } }" }""";
-
-		var channel = Channel.CreateBounded<int>(1);
-
-		List<Task> consumerTasks = new();
-		for (var i = 0; i < 4; i++)
+		var start = Stopwatch.GetTimestamp();
+		var batchStart = start;
+		
+		for (var i = 1; i < 100_001; i++)
 		{
-			consumerTasks.Add(Task.Run(async () =>
+			if (i % 5000 == 0)
 			{
-				while (await channel.Reader.WaitToReadAsync(ct) && channel.Reader.TryRead(out _))
-				{
-					try
-					{
-						HttpRequestMessage request = new()
-						{
-							RequestUri = new Uri("http://localhost:4161/graphql"),
-							Method = HttpMethod.Post,
-						};
+				Console.WriteLine($"Batch complete in {double.Round(Stopwatch.GetElapsedTime(batchStart).TotalSeconds, 1)} seconds ({i} / 100000)");
+				batchStart = Stopwatch.GetTimestamp();
+			}
+			
+			HttpRequestMessage request = new()
+			{
+				RequestUri = new Uri("http://localhost:4161/graphql"),
+				Method = HttpMethod.Post,
+			};
 
-						request.Content = new StringContent(query, Encoding.UTF8, "application/json");
+			request.Content = new StringContent(query, Encoding.UTF8, "application/json");
 
-						using var response = await client.SendAsync(request, ct);
+			using var response = await client.SendAsync(request, ct);
 
-						if (!response.IsSuccessStatusCode)
-						{
-							Console.WriteLine(await response.Content.ReadAsStringAsync(ct));
-						}
-						
-						response.EnsureSuccessStatusCode();
-					}
-					catch
-					{
-						// Errors (such as timeouts) are ignored
-					}
-				}
-			}, ct));
+			if (!response.IsSuccessStatusCode)
+			{
+				Console.WriteLine(await response.Content.ReadAsStringAsync(ct));
+			}
+			
+			response.EnsureSuccessStatusCode();
 		}
-		
-		for (var i = 0; i < 100_000; i++)
-		{
-			if (i % 5000 == 0) Console.WriteLine($"Completed {i}");
-			await channel.Writer.WriteAsync(i, ct);
-		}
-		
-		channel.Writer.Complete();
 
-		await Task.WhenAll(consumerTasks);
-		return "All requests finished.";
+		var result = $"All requests finished in {double.Round(Stopwatch.GetElapsedTime(start).TotalSeconds, 1)} seconds.";
+		Console.WriteLine(result);
+		return result;
 	}
 }
